6c1084f964bd5a5b5a6cd6db1d7f8eaa00051fc5
npybird <firstbird14120@gmail.com>
2026-05-31 13:03:15 +0700

    Initial Commit

---
+++ FocusFlowApp/FocusFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowAppApp.swift
+//  FocusFlowApp
+//
+//  Created by Natchanon Posayaanuwat on 31/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
